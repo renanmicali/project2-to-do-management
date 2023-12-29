1da4d490eac0540a8cdf8d23deda71e6ca6d667c
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-12-30 06:00:17 +1100

    updated NewProject/add to App.jsx

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,3 +1,5 @@
+import Input from "./Input.jsx";
+
 export default function NewProject() {
     return <div>
         <menu>
@@ -9,18 +11,9 @@ export default function NewProject() {
             </li>
         </menu>
         <div>
-            <p>
-                <label>Title</label>
-                <input />
-            </p>
-            <p>
-                <label>Description</label>
-                <textarea ></textarea> 
-            </p>
-            <p>
-                <label>Due Date</label>
-                <input />
-            </p>
+            <Input label="Title" />
+            <Input label="Description" textarea />
+            <Input label="Due Date" />
         </div>
     </div>
 }
