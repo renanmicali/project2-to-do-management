872fd1dce39bc143e3a3905ce1b5e42b9902dca5
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-12-30 05:55:00 +1100

    destructuring component

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,5 +1,6 @@
-export default function Input({textarea}) {
+export default function Input({label, textarea, ...props}) {
     return <p>
-        <label></label>
+        <label>{label}</label>
+        { textarea ? <textarea {...props} /> : <input {...props}/> }
     </p>
 }
