1f24ef9c5d6812cffe57b30573fa27b3913bb7e8
Renan Kozuka Micali <renanmicali@hotmail.com>
2024-01-01 04:53:40 +1100

    updating state/useRef

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,25 @@ function App() {
     });
 
   }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState)
+
   let content;
   if (projectsState.SelectedProjectId === null){
-    content = <NewProject/>
+    content = <NewProject onAdd={handleAddProject}/>
   } else if (projectsState.SelectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
   }
