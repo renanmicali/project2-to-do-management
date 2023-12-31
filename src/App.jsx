669fb50b5594b0d837e7d3909e6f52aca2ac8196
Renan Kozuka Micali <renanmicali@hotmail.com>
2024-01-01 04:22:59 +1100

    state added to app.jsx / changing page allowed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSideBar from "./components/ProjectsSideBar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    SelectedProjectId : undefined,
+    projects: []
+  });
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return{
+        ...prevState,
+        SelectedProjectId: null,
+      }
+    });
+
+  }
+  let content;
+  if (projectsState.SelectedProjectId === null){
+    content = <NewProject/>
+  } else if (projectsState.SelectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar/>
-      <NoProjectSelected/>
+      <ProjectsSideBar onStartAddProject={handleStartAddProject}/>
+      {content}
       </main>
   );
 }
